107d546f7d1788aa12cdd50bac040994db1a46ec
Zoinks <murarashuvlad@gmail.com>
2025-06-22 17:54:30 +0300

    Add camera bobbing features and related assets

---
+++ Assets/Scripts/Animation/CameraBobbingManager.cs
@@ -0,0 +1,37 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraBobbingManager : MonoBehaviour
+{
+    public new Rigidbody rigidbody;
+    public MovementManager movementManager;
+    public GroundedManager groundedManager;
+    public WallRunManager wallRunManager;
+
+    private CinemachineBasicMultiChannelPerlin _cinemachineBasicMultiChannelPerlin;
+
+    private void Awake()
+    {
+        _cinemachineBasicMultiChannelPerlin = GetComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    private void FixedUpdate()
+    {
+        var intensity = 0f;
+
+        if (groundedManager.IsGrounded || wallRunManager.IsWallRunning)
+        {
+            var rigidbodyHorizontalVelocity = new Vector3
+            {
+                x = rigidbody.linearVelocity.x,
+                z = rigidbody.linearVelocity.z
+            };
+
+            var speed = rigidbodyHorizontalVelocity.magnitude / movementManager.topSpeed;
+            intensity = Mathf.Clamp(speed, 0f, 1f);
+        }
+
+        _cinemachineBasicMultiChannelPerlin.AmplitudeGain = intensity;
+        _cinemachineBasicMultiChannelPerlin.FrequencyGain = intensity;
+    }
+}
