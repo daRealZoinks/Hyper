b95848f97b8a38090f0ad8f943a0dc3d9ced8bf6
Zoinks <murarashuvlad@gmail.com>
2025-07-05 14:30:50 +0300

    added InputManagerEditor and moved InputManager into the character controller folder

---
+++ Assets/Editor/InputManagerEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(InputManager))]
+public class InputManagerEditor : Editor
+{
+    private InputManager _inputManager;
+  
+    private void OnEnable()
+    {
+        _inputManager = (InputManager)target;
+    }
+
+    public override void OnInspectorGUI() { }
+}
