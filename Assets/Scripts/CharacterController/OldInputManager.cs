691135b2b38af26e5df88da4b664e151da5e857d
Zoinks <murarashuvlad@gmail.com>
2025-10-14 23:54:23 +0300

    deleted the fuckign shit ass visual scripting bullshet, man, like what the fuck bro, man, get the fuk outta here with taht fukin noodle ass shit that doesnt even know to cancel the fuckin input, you piece of bitch, what son of fuk did this shit, unity did done bought thsi fuk ass plugin and then went, here you go ghuys, get this unfinished piece of shit taht you need to manually update its nodes every fukin time you write one shit script, have fun dick face

---
+++ Assets/Scripts/CharacterController/OldInputManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(OldRigidbodyCharacterController))]
+public class OldInputManager : MonoBehaviour
+{
+    public GrapplingGun grapplingGun;
+
+    private OldRigidbodyCharacterController _rigidbodyCharacterController;
+
+    private void Awake()
+    {
+        _rigidbodyCharacterController = GetComponent<OldRigidbodyCharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        var moveInput = context.phase switch
+        {
+            InputActionPhase.Started or InputActionPhase.Performed => context.ReadValue<Vector2>(),
+            InputActionPhase.Disabled or InputActionPhase.Waiting or InputActionPhase.Canceled or _ => Vector2.zero,
+        };
+
+        _rigidbodyCharacterController.MoveInput = moveInput;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Started)
+        {
+            return;
+        }
+
+        _rigidbodyCharacterController.JumpPressed = true;
+    }
+
+    public void OnSlide(InputAction.CallbackContext context)
+    {
+        var slide = context.phase switch
+        {
+            InputActionPhase.Started or InputActionPhase.Performed => true,
+            InputActionPhase.Disabled or InputActionPhase.Waiting or InputActionPhase.Canceled or _ => false,
+        };
+
+        _rigidbodyCharacterController.Sliding = slide;
+    }
+
+    public void OnGrapple(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Disabled:
+                break;
+            case InputActionPhase.Waiting:
+                break;
+            case InputActionPhase.Started:
+                grapplingGun.StartGrapple();
+                break;
+            case InputActionPhase.Performed:
+                break;
+            case InputActionPhase.Canceled:
+                grapplingGun.StopGrapple();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Started)
+        {
+            return;
+        }
+
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(RigidbodyCharacterController))]
+public class InputManager : MonoBehaviour
+{
+    private RigidbodyCharacterController _rigidbodyCharacterController;
+
+    private void Awake()
+    {
+        _rigidbodyCharacterController = GetComponent<RigidbodyCharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        var moveInput = context.phase switch
+        {
+            InputActionPhase.Started or InputActionPhase.Performed => context.ReadValue<Vector2>(),
+            InputActionPhase.Canceled or InputActionPhase.Waiting or InputActionPhase.Disabled or _ => Vector2.zero,
+        };
+
+        _rigidbodyCharacterController.MoveInput = moveInput;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Started)
+        {
+            return;
+        }
+
+        _rigidbodyCharacterController.Jump();
+    }
+
+    public void OnSlide(InputAction.CallbackContext context)
+    {
+        var slide = context.phase switch
+        {
+            InputActionPhase.Started or InputActionPhase.Performed => true,
+            InputActionPhase.Canceled or InputActionPhase.Waiting or InputActionPhase.Disabled or _ => false,
+        };
+
+        _rigidbodyCharacterController.Sliding = slide;
+    }
+
+    public void OnGrapple(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Grapple started");
+        }
+
+        if (context.canceled)
+        {
+            Debug.Log("Grapple canceled");
+        }
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Started)
+        {
+            return;
+        }
+
+        // TODO: put this in another bigger component
+
+        Application.Quit();
+    }
+}
