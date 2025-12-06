08ca500eb31ac471735275daff8c39b3e92af3e0
Zoinks <murarashuvlad@gmail.com>
2025-12-06 02:43:31 +0200

    removed pakages and added keyboard and controller controls on the character select screen

--- Assets/Scripts/UI/Menus/CharacterSelectScreen.cs
+++ Assets/Scripts/UI/Menus/CharacterSelectScreen.cs
@@ -1,17 +1,34 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CharacterSelectScreen : MonoBehaviour
 {
     public Image characterProfilePicture;
-
     public TextMeshProUGUI characterName;
-
     public List<HyperCharacterInfo> hyperCharacterInfo;
 
-    private int currentCharacterIndex = 0;
+    public Button nextCharacterButton;
+    public Button previousCharacterButton;
+
+    private bool _isReady;
+    private int _currentCharacterIndex;
+
+    public bool IsReady
+    {
+        get
+        {
+            return _isReady;
+        }
+        set
+        {
+            _isReady = value;
+            nextCharacterButton.interactable = !_isReady;
+            previousCharacterButton.interactable = !_isReady;
+        }
+    }
 
     private void Start()
     {
@@ -20,20 +37,57 @@ public class CharacterSelectScreen : MonoBehaviour
 
     public void NextCharacter()
     {
-        currentCharacterIndex = (currentCharacterIndex + 1) % hyperCharacterInfo.Count;
-        UpdateCharacterDisplay();
+        if (!IsReady)
+        {
+            _currentCharacterIndex = (_currentCharacterIndex + 1) % hyperCharacterInfo.Count;
+            UpdateCharacterDisplay();
+        }
     }
 
     public void PreviousCharacter()
     {
-        currentCharacterIndex = (currentCharacterIndex - 1 + hyperCharacterInfo.Count) % hyperCharacterInfo.Count;
-        UpdateCharacterDisplay();
+        if (!IsReady)
+        {
+            _currentCharacterIndex = (_currentCharacterIndex - 1 + hyperCharacterInfo.Count) % hyperCharacterInfo.Count;
+            UpdateCharacterDisplay();
+        }
     }
 
     private void UpdateCharacterDisplay()
     {
-        var characterInfo = hyperCharacterInfo[currentCharacterIndex];
+        var characterInfo = hyperCharacterInfo[_currentCharacterIndex];
         characterProfilePicture.sprite = characterInfo.characterProfilePicture;
         characterName.text = characterInfo.characterName;
+    }
+
+    public void ToggleReady()
+    {
+        IsReady = !IsReady;
+    }
+
+    public void ChooseCharacter(InputAction.CallbackContext context)
+    {
+        if (context.performed && !IsReady)
+        {
+            var value = context.ReadValue<Vector2>();
+            _currentCharacterIndex = (_currentCharacterIndex + (int)value.normalized.x + hyperCharacterInfo.Count) % hyperCharacterInfo.Count;
+            UpdateCharacterDisplay();
+        }
+    }
+
+    public void ReadyUp(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsReady = true;
+        }
+    }
+
+    public void Unready(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsReady = false;
+        }
     }
 }
