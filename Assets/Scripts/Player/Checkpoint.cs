733ca6d46b4465bf51a4ebf3f2d2b71630b5a839
Zoinks <murarashuvlad@gmail.com>
2025-12-15 02:49:16 +0200

    made a circular test map and new checkpoint tech which is almost complete

---
+++ Assets/Scripts/Player/Checkpoint.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.TryGetComponent<PlayerProgress>(out var playerInterface))
+            {
+                playerInterface.SetCheckpoint(CheckpointManager.Instance.checkpointLinkedList.Find(this));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerProgress.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProgress : MonoBehaviour
+{
+    public LinkedListNode<Checkpoint> lastReachedCheckpoint;
+
+    public int lapsCompleted;
+    public int place;
+
+    private void Awake()
+    {
+        RaceManager.Instance.RegisterPlayer(this);
+    }
+
+    public void SetCheckpoint(LinkedListNode<Checkpoint> checkpointNode)
+    {
+        if (checkpointNode.Value == RaceManager.Instance.startingCheckpoint)
+        {
+            if (lastReachedCheckpoint == null)
+            {
+                lapsCompleted = 0;
+            }
+            else
+            {
+                if (checkpointNode == lastReachedCheckpoint.Next)
+                {
+                    LapCompleted();
+                }
+            }
+        }
+        else
+        {
+            if (checkpointNode == checkpointNode.List.Last)
+            {
+                // need to loop back to start
+            }
+        }
+
+        lastReachedCheckpoint = checkpointNode;
+    }
+
+    private void LapCompleted()
+    {
+        lapsCompleted++;
+    }
+}
